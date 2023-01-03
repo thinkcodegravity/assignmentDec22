832550887c30566f52d80236c22039433f7cca84
Pranjal-Oli <121149857+Pranjal-Oli@users.noreply.github.com>
2023-01-03 14:09:46 -0500

    Update Welcome.java

--- src/variables/Welcome.java
+++ src/variables/Welcome.java
@@ -4,6 +4,25 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		System.out.println("Assignments here");
+		int SSN;	// variablle to store SSN
+		String Product="Item name";//To store Product Details. Declare and initialize on same line
+		
+		String Lastname="ford";//To store last name.. Declare and next line initialize.
+		boolean Online = false; //To Store customer online status. Just Declare
+		double Pinum = 3.14 ; // To store value of pi . Just declare
+		long phonenumber =458654786;//To store customer phone number. Declare and initialize on same line
+		long cc=472433432;//To store customer credit card details. Just declare
+		String contactinfo="asdad@gmail.com";//To store company contact info. Just Declare
+		double height=5.7;//To store height. . Declare and initialize on same line
+		int empsalary=451565;//To store employee salary. Declare and next line initialize.
+		int item;//To store add 2 cart details. Just Declare
+		int payment;//To store payment details. Declare and next line initialize.
+		int bankbalance;//To store bank balance Just Declare
+		String companyname="  ";//To store company name Declare and initialize on same line
+		long bankaccount;//To store bank account number Just Declare
+		String shippingddress="  ";//To store shipping address Declare and initialize on same line
+
+
 	}
 
 }
