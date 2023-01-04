fd3260ed48bb694916bd18ee96ffdba1cfda2bd6
ab0691unt <abhiroseb@gmail.com>
2023-01-03 19:59:49 -0600

    DataStructureDesign
    Banking System design

---
+++ src/bankingSys/Addr.java
@@ -0,0 +1,15 @@
+package bankingSys;
+
+public class Addr {
+
+	public static void main(String[] args) {
+		int	streetNum;
+		String 	streetName;
+		String 	city;
+		String 	state;
+		int	zipcode;
+
+
+	}
+
+}
---
+++ src/bankingSys/Cust.java
@@ -0,0 +1,17 @@
+package bankingSys;
+
+public class Cust {
+
+	public static void main(String[] args) {
+		long	accNum;
+		String	custName;
+		Addr	address;
+		long	phoneNo;
+		long	custBalance;
+		Card	debitCard;
+		Card	creditCard;
+
+
+	}
+
+}
