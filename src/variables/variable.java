56e96dc1850273de3af1bbcf5640fb27d249abd4
bikesh268 <68455149+bikesh268@users.noreply.github.com>
2023-01-04 21:51:28 -0700

    Create variable.java

---
+++ src/variables/variable.java
@@ -0,0 +1,49 @@
+package variables;
+
+public class variable {
+	//to store Social security number.  Just do the declaration.
+	long SocialSecurityNumber;
+	//To store Product Details. Declare and initialize on same line
+	 float size=22 ;
+	 String color="red";
+	 int price=40;
+	//To store last name.. Declare and next line initialize.
+	 String lastName;
+	 
+	 LastName = "Obrien";
+	 
+	//To Store customer online status. Just Declare
+	 boolean OnlineStatus;
+	//To store value of pi . Just declare
+	long pi;
+	//To store customer phone number. Declare and initialize on same line
+	long PhoneNumber=72305674674;
+	//To store customer credit card details. Just declare
+	long CCNumber;
+	//To store company contact info. Just Declare
+	long contact;
+	//To store height. . Declare and initialize on same line
+	int height=22;
+	//To store employee salary. Declare and next line initialize.
+	float salary;
+	salary=50000.50;
+	//To store add 2 cart details. Just Declare
+	int cart1;
+	int cart2;
+	int add;
+	
+	//To store payment details. Declare and next line initialize.
+	Boolean payment;
+	payment=true;
+	//To store bank balance Just Declare
+	long bankBalance;
+	//To store company name Declare and initialize on same line
+	String companyName="ABC";
+	//To store bank account number Just Declare
+	long acNumber;
+	
+	//To store shipping address Declare and initialize on same line
+	String shipAddress="11100 E Dartmouth Ave";
+
+
+}
