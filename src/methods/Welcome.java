e02270bf9931570a20395d098b7e89ea8aef92ba
PranjalOli <121149857+Pranjal-Oli@users.noreply.github.com>
2023-01-05 01:21:28 -0500

    Update Welcome.java

--- src/methods/Welcome.java
+++ src/methods/Welcome.java
@@ -6,4 +6,52 @@ public class Welcome {
 		System.out.println("Assignments here");
 	}
 
+	public int payment() { //Create a method that can accept appropriate input for payment webpage and returns payment response.
+		return 0;
+	}
+	
+	public void registration() {//Create a method that can process registration
+	
+	}
+	
+	public float totalcartvalue( int quantity,float price){  //Create a method that can accept product input and calculate total cart value
+		return 0;
+	
+	}
+	
+	public double areaoftriangle(){//Create a method that can calculate area of rectangle
+		return 0;
+	}
+		
+	public double simpleinterest(){//Create a method that can calculate simple interest
+		return 0;
+	}
+		
+	public double withdraw()	{//Create a method that can process withdraw from bank account
+		return 0;
+	
+	}
+	
+	public double deposit(){//Create a method that can process deposit from bank account
+		return 0;
+	
+	}
+	
+	public double  electricityplan( int zipcode ){ //Create a method that can return electricity plan details if provided zip code as the input
+
+		return 0;
+
+	}
+
+	public double insurancedetail( int age ){//Create a method that can return insurance detail if provided input as age.
+		return 0;
+	}
+	
+	
+	public double totalamount( String product,float price , float percentage) {//Create a method that can return total amount if input provided is product price and discount percentage.
+		return 0;
+	}
+	public double withdraw( String userid , String password){//Create a method that can return user profile if provided customers userid and password as the input
+		return 0;
+	}
 }
