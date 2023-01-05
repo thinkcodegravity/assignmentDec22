bc4f2826f5e2aee947177929abbf19335fb5ecb4
raipramod <87155039+raipramod@users.noreply.github.com>
2023-01-04 23:30:35 -0700

    Create Variables.java

---
+++ src/variables/Variables.java
@@ -0,0 +1,67 @@
+package variables;
+
+//Name:Pramod Rai
+//Assignment:Variables
+//Instructor:John 
+//Class:Java Full Stack Trainings
+
+public class Variables {
+
+	public static void main(String[] args) {
+			//to store Social security number.  Just do the declaration.
+			long socialSecurityNumber;
+			
+			//To store Product Details. Declare and initialize on same line
+			 float size= 63.15f ;
+			 
+			 String myColor="white";
+			 int myPrice=50;
+			 
+			//To store last name.. Declare and next line initialize.
+			 String lastName= "Rai";
+			 
+			//To Store customer online status. Just Declare
+			 boolean OnlineStatus;
+			 
+			//To store value of pi . Just declare
+			double PI;
+			
+			//To store customer phone number. Declare and initialize on same line
+			String phoneNumber="1234567891";
+			
+			//To store customer credit card details. Just declare
+			long customerCreditNumber;
+			
+			//To store company contact info. Just Declare
+			long companyContact;
+			
+			//To store height. . Declare and initialize on same line
+			float height=5.3f;
+			
+			//To store employee salary. Declare and next line initialize.
+			float employerSalary;
+			
+			employerSalary=50000.50f;
+			
+			//To store add 2 cart details. Just Declare
+			int cartOne;
+			int cartTwo;
+			int add;
+			
+			//To store payment details. Declare and next line initialize.
+			Boolean payment;
+			payment=true;
+			
+			//To store bank balance Just Declare
+			long bankBalance;
+			
+			//To store company name Declare and initialize on same line
+			String companyName="WellsFargo";
+			
+			//To store bank account number Just Declare
+			long acNumber;
+			
+			//To store shipping address Declare and initialize on same line
+			String shipAddress="1234 S Parker Aurora, Co.";
+	}
+}
