f7621aee064705b9155964d3c6cf818140614f79
ab0691unt <abhiroseb@gmail.com>
2023-01-06 06:53:54 -0600

    pacticing references

---
+++ src/reference/Person.java
@@ -0,0 +1,16 @@
+package reference;
+
+public class Person {
+		int age=20;// default value 0
+		String name="john";
+		float weight=100.25f;
+	}
+	// variables : global, local, parametric
+	/*
+	  variable name, data type, values
+	  variable declaration,
+	  variable initialization
+	  variable assignment
+	  Java is CASE-SENSITIVE
+	*/
+
